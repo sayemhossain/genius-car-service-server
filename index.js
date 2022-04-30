aa7a88bbae052ac0a1af0fac31e4c7d09271fd3a
SAYAM HOSSAIN <shayem15-12011@diu.edu.bd>
2022-05-01 02:18:46 +0600

    new code added

--- index.js
+++ index.js
@@ -22,6 +22,7 @@ async function run() {
     await client.connect();
 
     const serviceCollection = client.db("geniusCar").collection("service");
+    const orderCollection = client.db("geniusCar").collection("order");
 
     app.get("/service", async (req, res) => {
       const query = {};
@@ -49,6 +50,21 @@ async function run() {
       const id = req.params.id;
       const query = { _id: ObjectId(id) };
       const result = await serviceCollection.deleteOne(query);
+      res.send(result);
+    });
+
+    //order collection api
+    app.get("/order", async (req, res) => {
+      const email = req.query.email;
+      const query = { email: email };
+      const cursor = orderCollection.find(query);
+      const orders = await cursor.toArray();
+      res.send(orders);
+    });
+
+    app.post("/order", async (req, res) => {
+      const order = req.body;
+      const result = await orderCollection.insertOne(order);
       res.send(result);
     });
   } finally {
