d95ca7d91aee9c4c2326eb28b0f5161e82315ee4
SAYAM HOSSAIN <shayem15-12011@diu.edu.bd>
2022-05-02 01:59:07 +0600

    new code

--- ___heroku___deploy.js
+++ ___heroku___deploy.js
@@ -1,4 +1,5 @@
 /**
+ * ---------------------------
  * ONE TIME FOR YOUR COMPUTER
  * ===========================
  * 1. create heroku account
@@ -6,6 +7,21 @@
  * 3. install heroku cli
  * 4. in comand line use "heroku login"
  *
+ *--------------------------
  * FOR EACH PROJECT ONE TIME
- *  1. heroku create
+ * --------------------------
+ * 1. heroku create
+ * 2. make sure you : git add  git commit and git push
+ * 3. git push heroku main
+ * 4. go to heroku current project > setting> Reveal config
+ * 5. copy paste config  vars from .env file
+ * -------------------
+ * UPDATE SERVER with new changes
+ * ------------------
+ * 1. make sure you : git add  git commit and git push
+ * 2. git push heroku main
+ * ----------------------
+ * CONNECT SERVER WITH CLINT
+ * ----------------------
+ *
  */
